434da254d40b0e0c4617b9bbb178bc8d047549c3
dhimasadip <dhimasadip.dap@gmail.com>
2020-06-19 22:25:15 +0700

    add home

--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
 const UserController = require('../controllers/UserController')
 
+router.get('/', (req,res) => {
+    res.status(200).json({author: 'Dhimas Adi Pangestu'})
+})
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.post('/google-sign-in', UserController.googleSignIn)
