f94f7c8c61b8ece383a2fc2efbb788add720ce69
dhimasadip <dhimasadip.dap@gmail.com>
2020-06-19 22:50:24 +0700

    setup

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
+
 require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
-const port = process.env.port || 4000
+const port = process.env.PORT || 4000
 
 
 app.use(cors())
